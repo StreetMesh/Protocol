960becb2c9f6ab33a09fa4bcdade72a1cd609846
collegeman <aaron@collegeman.net>
2025-04-10 15:36:43 -0400

    Add the word spatial to the summary, duh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ export default defineUserConfig({
   lang: 'en-US',
 
   title: 'Protocol',
-  description: 'A collection of Guides and APIs that establish the decentralized architecture of StreetMesh',
+  description: 'A collection of Guides and APIs that establish the decentralized spatial architecture of StreetMesh',
 
   theme: defaultTheme({
     logo: 'https://avatars.githubusercontent.com/u/206335802?s=400&u=df52d23f1e5fde57616a018ea44593df68293aa9&v=4',
